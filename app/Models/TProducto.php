4c593a6e6510b558290381b97df3d1cb0124819f
genos322 <genos322@gmail.com>
2023-01-01 22:41:09 -0500

    Feat: Creando modelos tventa y tproducto

---
+++ app/Models/TProducto.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TProducto extends Model
+{
+    protected $table = 'tproducto';
+    protected $primaryKey = 'idProducto';
+    public $incrementing = false;
+    public $timestamps = true;
+
+    public function tVenta()
+    {
+        return $this->hasMany(TVenta::class,'idProducto');
+    }
+
+}
+?>
---
+++ app/Models/TVenta.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TVenta extends Model
+{
+    protected $table = 'tventa';
+    protected $primaryKey = 'idVenta';
+    public $incrementing = false;
+    public $timestamps = true;
+    function tProducto()
+    {
+        return $this->belongsToMany(TProducto::class,'idVenta');
+    }
+
+}
+?>
